c3a65cb2e4d06d3555f0fa1759b1049945667db7
RQLMIUEE <riquelme.ct.santos@gmail.com>
2025-08-22 10:31:08 -0300

    adicionados classes pai e filhas

---
+++ src/ClasseAnimais.java
@@ -0,0 +1,21 @@
+
+public class ClasseAnimais {
+
+	String especieAt;
+	String nomeat;
+	int idadeat;
+	String corat;
+	float precoat;
+	
+	public ClasseAnimais() {
+		
+	}
+	
+	public ClasseAnimais(String especiePar, String nomePar, int idadePar, float precoPar, String corPar) {
+		this.nomeat = nomePar;
+		this.corat = corPar;
+		this.especieAt = especiePar;
+		this.idadeat = idadePar;
+		this.precoat = precoPar;
+	}
+}
